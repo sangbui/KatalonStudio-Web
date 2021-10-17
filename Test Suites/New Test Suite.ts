463ccfb89af7202d75749a819890e307a81ed528
Sang Bui <sang@sangbui.com>
2021-10-17 13:24:22 +0700

    Update Test Suites

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>de28c853-5d0b-4558-bd4f-82c73439aea1</testSuiteGuid>
+   <testCaseLink>
+      <guid>8735a4bd-ff8d-4dbd-9730-0875a1ad6f60</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_Comment_Invalid</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
